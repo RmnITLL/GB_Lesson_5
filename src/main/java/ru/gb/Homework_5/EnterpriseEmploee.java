2672886bbf6fb7268338451cda7bf222cf97c28b
Roman_Koloskov <inetmilo@rambler.ru>
2022-04-03 00:16:30 +0300

    add class EnterpriseEmployee

--- src/main/java/ru/gb/Homework_5/EnterpriseEmploee.java
+++ src/main/java/ru/gb/Homework_5/EnterpriseEmploee.java
@@ -1,6 +1,5 @@
 package ru.gb.Homework_5;
 
-import java.util.ArrayList;
 
 public class EnterpriseEmploee {
 
@@ -12,17 +11,17 @@ public class EnterpriseEmploee {
         Empls[0] = new Employee("Ivanov Ivan Ivanovich", "Engineer", "ivivan@mailbox.com",
                            "892312312", 30000, 30);
 
-        Empls[1] = new Employee("Petrov Petr Petrovich ", "Employee", "ivivan@mailbox.com",
-                "892312312", 30000, 41);
+        Empls[1] = new Employee("Petrov Petr Petrovich ", "Employee", "petrov41@mailbox.com",
+                "892412314", 31000, 41);
 
-        Empls[2] = new Employee("Petrov Petr Petrovich ", "Employee", "ivivan@mailbox.com",
-                "892312312", 30000, 45);
+        Empls[2] = new Employee("Sidorov Vasia Valerevich ", "Electric", "vsidorov@mailbox.com",
+                "892012315", 30500, 45);
 
-        Empls[3] = new Employee("Petrov Petr Petrovich ", "Employee", "ivivan@mailbox.com",
-                "892312312", 30000, 27);
+        Empls[3] = new Employee("Vasechkin Anton Pavlovich ", "Santehnik", "avasechkin@mailbox.com",
+                "892612311", 32000, 27);
 
-        Empls[4] = new Employee("Petrov Petr Petrovich ", "Design", "ivivan@mailbox.com",
-                "892312312", 30000, 40);
+        Empls[4] = new Employee("Fedorov Dmitrii Aleksandrovich ", "Doctor", "ivivan@mailbox.com",
+                "892812310", 33000, 40);
 
 
 
